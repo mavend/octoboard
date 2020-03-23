c4e2dbd10d8cb835a4ab443ea2abed987daf20f7
Maciej Głowacki <bombasarkadian@gmail.com>
2020-03-23 13:32:02 +0100

    Add simple loading screen

---
+++ src/Loading.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { 
+  Container,
+  Header,
+  Icon,
+  Message,
+} from "semantic-ui-react";
+
+const Loading = ({ G, ctx }) => {
+  const styles = {
+    mainHeader: {
+      marginTop: "20px",
+      marginBottom: "40px",
+    },
+    mainContent: {
+      marginLeft: "auto",
+      marginRight: "auto",
+    },
+    footer: {
+      marginTop: "20px",
+    }
+  }
+
+  return (
+    <div>
+      <Container>
+        <Header as="h1" textAlign="center" style={styles.mainHeader}>
+          <Icon name="pencil" /> Kalambury
+        </Header>
+      </Container>
+
+      <Container style={styles.mainContent}>
+        <Message icon>
+          <Icon name='circle notched' loading />
+          <Message.Content>
+            <Message.Header>Just one second</Message.Header>
+            We are connecting you to the game lobby.
+          </Message.Content>
+        </Message>
+      </Container>
+    </div>
+  )
+}
+
+export default Loading;
