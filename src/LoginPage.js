01f3e63aebc2729ca9d91996ea50e6d88c7dd03b
Jakub Kmiotek <jk@tb4hr.com>
2020-03-26 00:01:55 +0100

    Working firebase implementation

--- src/LoginPage.js
+++ src/LoginPage.js
@@ -1,18 +1,26 @@
 import React, {useContext, useState} from "react";
-import {Modal, Form, Image, Header, Button} from "semantic-ui-react";
+import {Modal, Form, Image, Header, Button, Message, Icon} from "semantic-ui-react";
 import {UserContext} from "./contexts/UserContext";
 
 const LoginPage = ({ open }) => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [error, setError] = useState(null);
 
-  const { login } = useContext(UserContext);
+  const { login, googleLogin } = useContext(UserContext);
 
   const handleLogin = async () => {
       try{
           await login(email, password)
       } catch (e) {
-          console.log(e);
+          const knownErrors = [
+              'auth/invalid-email',
+              'auth/wrong-password'
+          ];
+
+          if (knownErrors.indexOf(e.code) >= 0) {
+            setError(e.message);
+          }
       }
   };
 
@@ -23,27 +31,36 @@ const LoginPage = ({ open }) => {
         <Image wrapped size="medium" src="/images/game-hugo.png" />
         <Modal.Description>
           <Header>Login with your name</Header>
-            <Form onSubmit={handleLogin}>
+            <Form onSubmit={handleLogin} error={error}>
+                <Message
+                    error
+                    content={error}
+                />
+                <Form.Input
+                    autoFocus
+                    type="email"
+                    maxLength="24"
+                    placeholder='Email'
+                    name='Email'
+                    value={email}
+                    onChange={(_, {value}) => setEmail(value)}
+                />
+                <Form.Input
+                    placeholder='Password'
+                    type="password"
+                    name='Password'
+                    value={password}
+                    onChange={(_, {value}) => setPassword(value)}
+                />
                 <Form.Group>
-                    <Form.Input
-                        autoFocus
-                        type="email"
-                        maxLength="24"
-                        placeholder='Email'
-                        name='Email'
-                        value={email}
-                        onChange={(_, {value}) => setEmail(value)}
-                    />
-                    <Form.Input
-                        placeholder='Password'
-                        type="password"
-                        name='Password'
-                        value={password}
-                        onChange={(_, {value}) => setPassword(value)}
-                    />
                     <Form.Button content='Login' />
                 </Form.Group>
             </Form>
+            <Button content='Register'/>
+            <Button onClick={googleLogin}>
+                <Icon name={"google"}/>
+            </Button>
+
         </Modal.Description>
       </Modal.Content>
     </Modal>
