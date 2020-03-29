e19e69c5e7e302a2d1953f629de7a633c0816343
Jakub Kmiotek <jk@tb4hr.com>
2020-03-29 04:19:48 +0200

    Add Routers, Integrate fronted with firebase

--- src/games/Games.js
+++ src/games/Games.js
@@ -2,3 +2,5 @@ import { Kalambury } from "./kalambury/Kalambury";
 import KalamburyBoard from "./kalambury/Board";
 
 export const KalamburyComponent = { game: Kalambury, board: KalamburyBoard };
+
+export const gameComponents = [KalamburyComponent];
---
+++ src/services/User.js
@@ -0,0 +1,87 @@
+import FirebaseClient from "./Firebase";
+
+export class UserClient {
+    constructor(updater) {
+        this.user = null;
+        this.updateUser = () => {
+            updater(this.user);
+        };
+
+        this.login = this.login.bind(this);
+        this.googleLogin = this.googleLogin.bind(this);
+        this.logout = this.logout.bind(this);
+        this.init();
+    }
+
+    async login(email, password ) {
+        const response = await FirebaseClient.auth().signInWithEmailAndPassword(email, password);
+        if (response.user) {
+            this.user = response.user;
+            return response;
+        } else {
+            throw new Error('No user in response')
+        }
+    }
+
+    async googleLogin() {
+        const provider = new FirebaseClient.auth.GoogleAuthProvider();
+        provider.addScope('email');
+        provider.addScope('profile');
+        provider.setCustomParameters({ prompt: 'select_account' });
+
+        const response = await FirebaseClient.auth().signInWithPopup(provider);
+        if (response.user) {
+            this.user = response.user;
+        } else {
+            throw new Error('No user in response')
+        }
+    };
+
+    async logout() {
+        try {
+            await FirebaseClient.auth().signOut();
+            this.user = null;
+            this.updateUser();
+            return true;
+        } catch(error) {
+            return error;
+        }
+    }
+
+    async register(email, password) {
+        await FirebaseClient.auth().createUserWithEmailAndPassword(email, password);
+        return await this.login(email, password);
+    }
+
+    async changePassword(password) {
+        const user = FirebaseClient.auth().currentUser;
+        if (user) {
+            return user.updatePassword(password);
+        }
+        return Promise.reject('Something went wrong');
+    };
+
+    async sendResetPasswordLink(email) {
+        return FirebaseClient.auth().sendPasswordResetEmail(email);
+    };
+
+    init() {
+        FirebaseClient.auth().onAuthStateChanged(async user => {
+            if (user) {
+                this.user = user;
+                this.updateUser();
+            } else {
+                this.logout();
+            }
+        });
+
+        FirebaseClient.auth().onIdTokenChanged(async user => {
+            if (user) {
+                this.user = user;
+                this.updateUser();
+            } else {
+                this.logout();
+            }
+        });
+    }
+}
