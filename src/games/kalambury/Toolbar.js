23b1a05ccd3819b09a427e2933413ba10beb8296
Maciej Głowacki <bombasarkadian@gmail.com>
2020-03-31 22:40:40 +0200

    Adapt Kalambury board to smaller screens

--- src/games/kalambury/Toolbar.js
+++ src/games/kalambury/Toolbar.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Icon, Menu, Popup } from "semantic-ui-react";
+import { Responsive, Icon, Menu, Popup } from "semantic-ui-react";
 import { useTranslation } from "react-i18next";
 import { COLORS } from "config/constants";
 
@@ -27,48 +27,90 @@ const Toolbar = ({
   };
 
   return (
-    <Menu borderless size="small" style={{ height: "41px" }}>
-      {colors.map((color) => (
-        <Menu.Item
-          key={color}
-          name={`color-${color}`}
-          active={currentColor === color}
-          onClick={() => onClickColor(color)}
-        >
-          <ColorBox color={color} />
-        </Menu.Item>
-      ))}
+    <>
+      <Responsive as={Menu} minWidth={660} borderless size="small">
+        {colors.map((color) => (
+          <Menu.Item
+            key={color}
+            name={`color-${color}`}
+            active={currentColor === color}
+            onClick={() => onClickColor(color)}
+          >
+            <ColorBox color={color} />
+          </Menu.Item>
+        ))}
 
-      <Menu.Item name="eraser" active={currentColor === eraserColor} onClick={onClickEraser}>
-        <Icon fitted name="eraser" />
-      </Menu.Item>
-      <Menu.Menu position="right">
-        <Popup
-          content={t("tools.undo")}
-          trigger={
-            <Menu.Item name="undo" active={false} disabled={!canUndo} onClick={onUndoDrawing}>
-              <Icon fitted name="undo" />
+        <Menu.Item name="eraser" active={currentColor === eraserColor} onClick={onClickEraser}>
+          <Icon fitted name="eraser" />
+        </Menu.Item>
+        <Menu.Menu position="right">
+          <Popup
+            content={t("tools.undo")}
+            trigger={
+              <Menu.Item name="undo" active={false} disabled={!canUndo} onClick={onUndoDrawing}>
+                <Icon fitted name="undo" />
+              </Menu.Item>
+            }
+          />
+          <Popup
+            content={t("tools.clear")}
+            trigger={
+              <Menu.Item name="trash" active={false} onClick={onClearAll}>
+                <Icon fitted color="red" name="trash alternate outline" />
+              </Menu.Item>
+            }
+          />
+          <Popup
+            content={t("tools.forfeit")}
+            trigger={
+              <Menu.Item name="flag" active={false} onClick={onForfeit}>
+                <Icon fitted color="red" name="flag" />
+              </Menu.Item>
+            }
+          />
+        </Menu.Menu>
+      </Responsive>
+      <Responsive maxWidth={659}>
+        <Menu widths={2} size="small">
+          <Menu.Item name="trash" active={false} onClick={onClearAll}>
+            <Icon fitted color="red" name="trash alternate outline" />
+          </Menu.Item>
+          <Menu.Item name="flag" active={false} onClick={onForfeit}>
+            <Icon fitted color="red" name="flag" />
+          </Menu.Item>
+        </Menu>
+        <Menu widths={6} attached="top" size="small">
+          {colors.slice(0, 6).map((color) => (
+            <Menu.Item
+              key={color}
+              name={`color-${color}`}
+              active={currentColor === color}
+              onClick={() => onClickColor(color)}
+            >
+              <ColorBox color={color} />
             </Menu.Item>
-          }
-        />
-        <Popup
-          content={t("tools.clear")}
-          trigger={
-            <Menu.Item name="trash" active={false} onClick={onClearAll}>
-              <Icon fitted color="red" name="trash alternate outline" />
+          ))}
+        </Menu>
+        <Menu widths={6} attached="bottom" size="small">
+          {colors.slice(6, 10).map((color) => (
+            <Menu.Item
+              key={color}
+              name={`color-${color}`}
+              active={currentColor === color}
+              onClick={() => onClickColor(color)}
+            >
+              <ColorBox color={color} />
             </Menu.Item>
-          }
-        />
-        <Popup
-          content={t("tools.forfeit")}
-          trigger={
-            <Menu.Item name="flag" active={false} onClick={onForfeit}>
-              <Icon fitted color="red" name="flag" />
-            </Menu.Item>
-          }
-        />
-      </Menu.Menu>
-    </Menu>
+          ))}
+          <Menu.Item name="eraser" active={currentColor === eraserColor} onClick={onClickEraser}>
+            <Icon fitted name="eraser" />
+          </Menu.Item>
+          <Menu.Item name="undo" active={false} disabled={!canUndo} onClick={onUndoDrawing}>
+            <Icon fitted name="undo" />
+          </Menu.Item>
+        </Menu>
+      </Responsive>
+    </>
   );
 };
 
