403e897ee98af94addc3ac97ff530a2cccc124b0
Patryk Pastewski <past.patryk@gmail.com>
2020-03-31 10:41:28 +0200

    More descriptive API error messages

--- src/services/API.js
+++ src/services/API.js
@@ -4,7 +4,12 @@ export const apiRequests = {
   fetchRooms: async (games) => {
     const urls = games.map((game) => roomsUrl(game.name));
     const responses = await Promise.all(urls.map((url) => fetch(url)));
-    const jsons = await Promise.all(responses.map(async (res) => await res.json()));
+    const invalidResponses = responses.filter((response) => !response.ok);
+    if (invalidResponses.length > 0) {
+      const errorMessages = await Promise.all(invalidResponses.map((response) => response.text()));
+      throw new Error(errorMessages);
+    }
+    const jsons = await Promise.all(responses.map((res) => res.json()));
     const rooms = jsons
       .map((res, idx) =>
         res.rooms.map((room) => {
@@ -16,7 +21,8 @@ export const apiRequests = {
   },
   fetchRoom: async (gameName, gameID) => {
     const response = await fetch(roomUrl(gameName, gameID));
-    return await response.json();
+    if (response.ok) return await response.json();
+    throw new Error(await response.text());
   },
   createRoom: async (gameName, numPlayers, setupData) => {
     const response = await fetch(createGameUrl(gameName), {
@@ -27,7 +33,8 @@ export const apiRequests = {
       }),
       headers: { "Content-Type": "application/json" },
     });
-    return await response.json();
+    if (response.ok) return await response.json();
+    throw new Error(await response.text());
   },
   joinRoom: async (gameName, gameID, playerID, playerName) => {
     const response = await fetch(joinRoomUrl(gameName, gameID), {
@@ -38,7 +45,8 @@ export const apiRequests = {
       }),
       headers: { "Content-Type": "application/json" },
     });
-    return await response.json();
+    if (response.ok) return await response.json();
+    throw new Error(await response.text());
   },
   leaveGame: async (gameName, gameID, playerID, credentials) => {
     const response = await fetch(leaveGameUrl(gameName, gameID), {
@@ -49,6 +57,7 @@ export const apiRequests = {
       }),
       headers: { "Content-Type": "application/json" },
     });
-    return await response.json();
+    if (response.ok) return await response.json();
+    throw new Error(await response.text());
   },
 };
