ca0eab935b6a1d59a39a8874716fe64661457a5e
Patryk Pastewski <past.patryk@gmail.com>
2020-10-21 16:45:39 +0200

    Fix lobby client

--- src/services/LobbyClient.js
+++ src/services/LobbyClient.js
@@ -6,6 +6,6 @@ export const lobbyClient = new LobbyClient({ server: API_ROOT });
 lobbyClient.listAllMatches = async (games) => {
   const responses = await Promise.all(games.map((game) => lobbyClient.listMatches(game.name)));
   return responses
-    .map((matches, idx) => matches.map((match) => ({ ...match, gameName: games[idx].name })))
+    .map(({ matches }, idx) => matches.map((match) => ({ ...match, gameName: games[idx].name })))
     .flat();
 };
