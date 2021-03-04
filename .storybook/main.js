5cd37c2aea2e22d83cfc8347de942d62a5cbddf6
Maciej Głowacki <bombasarkadian@gmail.com>
2021-03-04 11:09:26 +0100

    Fix storybooks

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,11 @@
 module.exports = {
-  stories: ["../src/**/*.stories.js"],
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
     "@storybook/preset-create-react-app",
     "@storybook/addon-actions",
+    "@storybook/addon-links",
     "@storybook/addon-storysource",
+    "@storybook/addon-essentials",
     "@storybook/addon-knobs/register",
-    "@storybook/addon-viewport/register",
   ],
 };
--- src/components/game/WaitingBoard/index.stories.js
+++ src/components/game/WaitingBoard/index.stories.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { action } from "@storybook/addon-actions";
-import { boolean, text } from "@storybook/addon-knobs";
 
 import { gameDecorator } from "../GameContextMock";
 import WaitingBoard from ".";
